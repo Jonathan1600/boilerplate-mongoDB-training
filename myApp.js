6b66f4139e6fb167af5aaf576ebc41d63f09765c
Jonathan1600 <70250506+Jonathan1600@users.noreply.github.com>
2020-11-24 19:46:53 -0700

    finding by id editing and saving it

--- myApp.js
+++ myApp.js
@@ -49,8 +49,13 @@ const findPersonById = (personId, done) => {
 
 const findEditThenSave = (personId, done) => {
   const foodToAdd = "hamburger";
-
-  done(null /*, data*/);
+  Person.findById(personId, (err, person) => {
+    person.favoriteFoods.push(foodToAdd);
+    person.save((err, data) => {
+      if (err) return console.error(err);
+      done(null, data);
+    });
+  })
 };
 
 const findAndUpdate = (personName, done) => {
